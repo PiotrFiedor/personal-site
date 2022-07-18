2e8c6cdf191c7a661b42f190ab0f788c2b690cd9
PiotrFiedor <107081558+PiotrFiedor@users.noreply.github.com>
2022-07-18 23:13:42 +0200

    V2 Cezar persolna site

--- index.js
+++ index.js
@@ -2,16 +2,16 @@
 // Replace the arguments below according to your preference.
 
 // space, scary, military, romantic, cowboy, fantasy, superhero
-favouriteMovieGenre("space")
+favouriteMovieGenre("superhero")
 
 // watermelon, tomato, banana, orange, avocado, blueberry
 favouriteFruit("avocado")
 
 // light, dark
-favouriteMode("dark")
+favouriteMode("light")
 
 // sharp, soft, round
-favouriteEdgeStyle("soft")
+favouriteEdgeStyle("round")
 
 
 
